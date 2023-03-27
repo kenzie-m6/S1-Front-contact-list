9b4c711757f707aff3b8308d3257b9f0ed11c350
unknown <dreicleal@hotmail.com>
2023-03-27 06:29:39 -0300

    crud contacts

--- contacts-list/src/components/RegisterForm/index.tsx
+++ contacts-list/src/components/RegisterForm/index.tsx
@@ -3,8 +3,6 @@ import { SubmitHandler, useForm } from "react-hook-form";
 import { IRegisterFormValues } from "../../interfaces/userInterfaces";
 import { UserContext } from "../../providers/UserContext";
 import { Input } from "../Input";
-import { zodResolver } from "@hookform/resolvers/zod";
-import { schemaRegister } from "../../schemas/schemaRegister";
 
 export const RegisterForm = () => {
   const {
@@ -15,14 +13,11 @@ export const RegisterForm = () => {
   const { userRegister } = useContext(UserContext);
 
   const submit: SubmitHandler<IRegisterFormValues> = (formData) => {
-    console.log(`Form Data-> ${formData}`) 
     userRegister(formData);
   };
-
   return (
-
       <form onSubmit={handleSubmit(submit)}>
-        <Input type="text" label="Nome completo" register={register("fullName")} error={errors.fullName} />
+          <Input type="text" label="Nome completo" register={register("fullName")} error={errors.fullName} />
           <Input type="email" label="Seu e-mail" register={register("email")} error={errors.email} />
           <Input type="email" label="E-mail secundário" register={register("secondaryEmail")} error={errors.secondaryEmail} />
           <Input type="password" label="Crie uma senha" register={register("password")} error={errors.password} />
--- contacts-list/src/pages/DashboardPage/index.tsx
+++ contacts-list/src/pages/DashboardPage/index.tsx
@@ -1,4 +1,21 @@
+import { useContext } from "react"
+import { Contacts } from "../../components/ContactsContainer"
+import { ContactsProvider } from "../../providers/ContactsContext"
+import { UserContext } from "../../providers/UserContext"
 
 export const DashBoardPage = () =>{
-    
+    const {user} = useContext(UserContext)
+
+    return (
+        <>
+            <div>
+                <h2>Olá, {user?.fullName}</h2>
+                <p>Seu e-mail: {user?.email}</p>
+                <p>Seu Telefone: {user?.phone}</p>
+            </div>
+            <ContactsProvider>
+                <Contacts/>
+            </ContactsProvider>
+        </>
+    )
 }
